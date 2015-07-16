03e8190413abd28bd8b8bd7290438c532d1fe00d
mazder <p3sp4@unb.ca>
2015-07-16 11:29:21 -0300

    Prefix Error Checking is done
    For every operation prefix error checking is added, however, this is not
    part of Simulator
    Implementation of reading trace file 3.0 is done, need to run GC and get
    results....

--- Debug/Main/Object.d
+++
@@ -1,3 +0,0 @@
-Main/Object.d Main/Object.o: ../Main/Object.cpp ../Main/Object.hpp
-
-../Main/Object.hpp:
--- Debug/Main/ObjectContainer.d
+++
@@ -1,9 +0,0 @@
-Main/ObjectContainer.d Main/ObjectContainer.o: \
- ../Main/ObjectContainer.cpp ../Main/ObjectContainer.hpp \
- ../Main/Object.hpp ../Main/../defines.hpp
-
-../Main/ObjectContainer.hpp:
-
-../Main/Object.hpp:
-
-../Main/../defines.hpp:
